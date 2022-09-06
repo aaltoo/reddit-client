0cce73383b2098ff69eaec470e707c9d7e748267
aaltoo <scotlandfjord@protonmail.com>
2022-09-06 14:51:44 +0900

    Try to fix env vars

--- src/api/requester.ts
+++ src/api/requester.ts
@@ -1,8 +1,8 @@
 import Snoowrap from "snoowrap";
 
 export const requester = new Snoowrap({
-  userAgent: import.meta.env.VITE_USER_AGENT,
-  clientId: import.meta.env.VITE_CLIENT_ID,
-  clientSecret: import.meta.env.VITE_CLIENT_SECRET,
+  userAgent: import.meta.env.VITE_USER_AGENT || process.env.USER_AGENT,
+  clientId: import.meta.env.VITE_CLIENT_ID || process.env.CLIENT_ID,
+  clientSecret: import.meta.env.VITE_CLIENT_SECRET || process.env.CLIENT_SECRET,
   refreshToken: localStorage.getItem("refresh_token")!,
 });
