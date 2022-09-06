35d04faaaf78be2fba8181d25bbd929f12904798
aaltoo <scotlandfjord@protonmail.com>
2022-09-06 18:31:14 +0900

    Return lazy loading

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,4 @@
-import Subreddit from "@/views/SubredditView.vue";
-import HomeView from "@/views/HomeView.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import SubmissionView from "../views/SubmissionView.vue";
-import LoginView from "@/views/LoginView.vue";
-import CallbackView from "@/views/CallbackView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,17 +6,17 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: () => import("../views/HomeView.vue"),
     },
     {
       path: "/r/:subredditName/",
       name: "subreddit",
-      component: Subreddit,
+      component: () => import("../views/SubredditView.vue"),
     },
     {
       path: "/r/:subredditName/comments/:postId/:postTitle?",
       name: "comment",
-      component: SubmissionView,
+      component: () => import("../views/SubmissionView.vue"),
     },
     {
       path: "/auth",
@@ -29,12 +24,12 @@ const router = createRouter({
         {
           path: "login",
           name: "login",
-          component: LoginView,
+          component: () => import("../views/LoginView.vue"),
         },
         {
           path: "callback",
           name: "callback",
-          component: CallbackView,
+          component: () => import("../views/CallbackView.vue"),
         },
       ],
     },
